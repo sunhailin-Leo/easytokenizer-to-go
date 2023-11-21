343aa411d4c4c194feacb505860ac82637bfdb57
github-action-benchmark <github@users.noreply.github.com>
2023-11-21 03:17:14 +0000

    add Benchmark (go) benchmark result for bab7aede9b6bbfab681aab622a8479cc5e968d58

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700536606545,
+  "lastUpdate": 1700536634445,
   "repoUrl": "https://github.com/sunhailin-Leo/easytokenizer-to-go",
   "entries": {
     "Benchmark": [
@@ -60,6 +60,66 @@ window.BENCHMARK_DATA = {
             "value": 10459,
             "unit": "ns/op\t    1056 B/op\t      40 allocs/op",
             "extra": "114783 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "379978424@qq.com",
+            "name": "LeoSun",
+            "username": "sunhailin-Leo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bab7aede9b6bbfab681aab622a8479cc5e968d58",
+          "message": "Merge pull request #8 from sunhailin-Leo/dependabot/github_actions/securego/gosec-2.18.2\n\nBump securego/gosec from 2.18.0 to 2.18.2",
+          "timestamp": "2023-11-21T11:16:16+08:00",
+          "tree_id": "e6bc093390ecd8aeb078ff305c3ef7a1f98be0f3",
+          "url": "https://github.com/sunhailin-Leo/easytokenizer-to-go/commit/bab7aede9b6bbfab681aab622a8479cc5e968d58"
+        },
+        "date": 1700536634024,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkChineseEncode",
+            "value": 1692,
+            "unit": "ns/op\t     192 B/op\t       1 allocs/op",
+            "extra": "755377 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkChineseEncodeWithIds",
+            "value": 2538,
+            "unit": "ns/op\t     752 B/op\t      12 allocs/op",
+            "extra": "417639 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkChineseWordPieceTokenize",
+            "value": 3118,
+            "unit": "ns/op\t     440 B/op\t      21 allocs/op",
+            "extra": "380635 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThaiEncode",
+            "value": 7819,
+            "unit": "ns/op\t     256 B/op\t       1 allocs/op",
+            "extra": "141057 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThaiEncodeWithIds",
+            "value": 9113,
+            "unit": "ns/op\t    1104 B/op\t      12 allocs/op",
+            "extra": "127099 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThaiWordPieceTokenize",
+            "value": 11032,
+            "unit": "ns/op\t    1056 B/op\t      40 allocs/op",
+            "extra": "112510 times\n4 procs"
           }
         ]
       }
